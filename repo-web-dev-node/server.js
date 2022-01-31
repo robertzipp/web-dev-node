c6c1140f392a4f182e367685e677adcdc79bf19e
Zobby <zipprobertt@gmail.com>
2022-01-31 12:05:55 -0800

    node upload

---
+++ repo-web-dev-node/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers",
+               "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+               "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
+
+require('./services/movie-service')(app);
+
+
+require('./services/tweeter-service')(app);
+
+app.listen(4000);
